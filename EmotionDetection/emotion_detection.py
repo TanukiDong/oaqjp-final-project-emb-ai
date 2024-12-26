b8d6f9975960f5d6b2edd932319050be632e7bbb
TanukiDong <eurondelta@gmail.com>
2024-12-26 08:37:49 -0500

    Initial Commit

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,41 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    data = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json=data, headers=header)
+    formatted_response = json.loads(response.text)
+
+    if response.status_code != 400:
+        return {
+        'anger': None,
+        'disgust': None,
+        'fear': None,
+        'joy': None,
+        'sadness': None,
+        'dominant_emotion': None
+    }
+
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    emotions = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+
+    dominant_emotion = max(emotions, key=emotions.get)
+    
+    return emotions | {'dominant_emotion': dominant_emotion}
+
+# from emotion_detection import emotion_detector
+# emotion_detector("I love this new technology.")
+
